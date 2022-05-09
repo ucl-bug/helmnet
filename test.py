71c8774bf982fd8a1a58b82530324d7314bfbba8
Yaniv Khaslavsky <45836033+ykhas@users.noreply.github.com>
2022-05-09 12:34:14 -0400

    Refactored test.py into modular format

--- test.py
+++ test.py
@@ -1,31 +1,38 @@
-from helmnet import IterativeSolver
+from helmnet import IterativeSolver, load_settings
 from helmnet.support_functions import fig_generic
 import numpy as np
-import torch
 
-solver = IterativeSolver.load_from_checkpoint(
-    "checkpoints/trained_weights.ckpt", strict=False
-)
-solver.freeze()  # To evaluate the model without changing it
-solver.to("cuda:0")
+def sample_prediction(useCuda: bool = False) :
+    settings = load_settings("experiments/base.json")
 
-# Setup problem
-source_location = [30, 128]
-sos_map = np.ones((256, 256))
-sos_map[100:170, 30:240] = np.tile(np.linspace(2,1,210),(70,1))
+    solver = IterativeSolver.load_from_checkpoint(
+        checkpoint_path="checkpoints/trained_weights.ckpt", strict=False, test_data_path=settings["medium"]["test_set"]
+    )
+    solver.freeze()  # To evaluate the model without changing it
 
-# Set model domain size (assumed square)
-solver.set_domain_size(sos_map.shape[-1], source_location=source_location)
+    if useCuda:
+        solver.to("cuda:0")
 
-# Run example in kWave and pytorch, and produce figure
-fig_generic(
-    solver,
-    sos_map,
-    path="images/withgmres",
-    source_location=source_location,
-    omega=1,
-    min_sos=1,
-    cfl=0.1,
-    roundtrips=10.0,
-    mode="normal",
-)
+    # Setup problem
+    source_location = [30, 128]
+    sos_map = np.ones((256, 256))
+    sos_map[100:170, 30:240] = np.tile(np.linspace(2,1,210),(70,1))
+
+    # Set model domain size (assumed square)
+    solver.set_domain_size(sos_map.shape[-1], source_location=source_location)
+
+    # Run example in kWave and pytorch, and produce figure
+    fig_generic(
+        solver,
+        sos_map,
+        path="images/withgmres",
+        source_location=source_location,
+        omega=1,
+        min_sos=1,
+        cfl=0.1,
+        roundtrips=10.0,
+        mode="normal",
+    )
+
+if __name__ == "__main__":
+    sample_prediction()
